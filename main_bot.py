eebf858a8ea6c4367d6792031b4b245d99a7ea23
Legan <legan.16@rambler.ru>
2023-04-14 23:53:58 +0300

    New exrcise

--- main_bot.py
+++ main_bot.py
@@ -1,27 +1,136 @@
 # Link to the bot: https://t.me/innopolis_testing_bot
-import sys
-import locale
-import re
 
+from config import TOKEN
 
 from aiogram.types import BotCommand, BotCommandScopeDefault
 from aiogram.dispatcher import filters
 from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from aiogram.utils.exceptions import BadRequest
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import Dispatcher
 from aiogram.dispatcher.filters.builtin import ChatType
 from aiogram.dispatcher.middlewares import BaseMiddleware
 from aiogram.dispatcher.handler import CancelHandler
 
-
-
 # Импортируем функции из файла с базой данных
-from database import insert_user_db, delete_user_db, update_user_db, get_nickname_db, is_banned, ban_user, unban_user
+import sqlite3
 
 
-TOKEN = "6273983990:AAGNUQpjEen2GKcfJYtcHygvolZkzxg8Fpk"
+# Создаем соединение с базой данных
+conn = sqlite3.connect("users_telegram.db")
+
+# Создаем курсор для выполнения SQL-запросов
+cur = conn.cursor()
+
+# Создаем таблицу users, если она еще не существует
+cur.execute("CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY, nickname TEXT, blacklist bool)")
+
+# Функция проверки нахождения юзера в бд
+def check_user_in_db(user_id):
+    # Проверяем, есть ли уже такой пользователь в таблице
+    cur.execute("SELECT * FROM users WHERE user_id = ?", (user_id,))
+    user = cur.fetchone()
+    if user is None:
+        return False
+    return True
+
+
+# Функция для записи данных пользователя в таблицу
+def insert_user_db(user_id, nickname):
+    # Проверяем, есть ли уже такой пользователь в таблице
+    cur.execute("SELECT * FROM users WHERE user_id = ?", (user_id,))
+    user = cur.fetchone()
+    if user is None:
+        # Если нет, то добавляем его в таблицу
+        cur.execute("INSERT INTO users (user_id, nickname) VALUES (?, ?)", (user_id, nickname))
+        conn.commit()
+        print(f"Пользователь {nickname} добавлен в базу данных.")
+    else:
+        # Если есть, то выводим сообщение об ошибке
+        print(f"Пользователь {nickname} уже есть в базе данных.")
+
+# Функция для удаления данных пользователя из таблицы
+def delete_user_db(user_id):
+    # Проверяем, есть ли такой пользователь в таблице
+    cur.execute("SELECT * FROM users WHERE user_id = ?", (user_id,))
+    user = cur.fetchone()
+    if user is not None:
+        # Если есть, то удаляем его из таблицы
+        cur.execute("DELETE FROM users WHERE user_id = ?", (user_id,))
+        conn.commit()
+        print(f"Пользователь {user[1]} удален из базы данных.")
+    else:
+        # Если нет, то выводим сообщение об ошибке
+        print(f"Пользователя с таким id нет в базе данных.")
+
+# Функция для обновления данных пользователя в таблице
+def update_user_db(user_id, new_nickname):
+    # Проверяем, есть ли такой пользователь в таблице
+    cur.execute("SELECT * FROM users WHERE user_id = ?", (user_id,))
+    user = cur.fetchone()
+    if user is not None:
+        # Если есть, то обновляем его никнейм в таблице
+        cur.execute("UPDATE users SET nickname = ? WHERE user_id = ?", (new_nickname, user_id))
+        conn.commit()
+        print(f"Пользователь {user[1]} обновлен на {new_nickname}.")
+    else:
+        # Если нет, то выводим сообщение об ошибке
+        print(f"Пользователя с таким id нет в базе данных.")
+
+# Создаем функцию для получения никнейма из базы данных по user_id
+def get_nickname_db(user_id):
+    # Создаем подключение к базе данных
+    # Пытаемся найти запись в таблице users по user_id
+    cur.execute("SELECT nickname FROM users WHERE user_id = ?", (user_id,))
+    row = cur.fetchone()
+    # Если запись найдена, возвращаем никнейм
+    if row:
+        nickname = row[0]
+        return nickname
+    # Если запись не найдена, возвращаем None
+    else:
+        return None
+    
+# Функция для проверки, находится ли пользователь в черном списке
+async def is_banned(user_id):
+    # Формируем SQL-запрос для получения значения столбца blacklist по user_id
+    sql = f"SELECT blacklist FROM users WHERE user_id = {user_id};"
+    # Выполняем запрос и получаем результат
+    cur.execute(sql)
+    result = cur.fetchone()
+    # Если результат не пустой, возвращаем значение столбца blacklist (True или False)
+    if result:
+        return result[0]
+    # Иначе возвращаем False, так как пользователь не находится в базе данных
+    else:
+        return False
+
+# Функция для добавления пользователя в черный список
+async def ban_user(user_id):
+    # Проверяем, есть ли пользователь в базе данных
+    if await is_banned(user_id) is False:
+        # Если нет, то добавляем его в таблицу users с значением столбца blacklist в True
+        sql = f"INSERT INTO users (user_id, blacklist) VALUES ({user_id}, True);"
+        cur.execute(sql)
+        conn.commit()
+    else:
+        # Если есть, то обновляем значение столбца blacklist в True
+        sql = f"UPDATE users SET blacklist = True WHERE user_id = {user_id};"
+        cur.execute(sql)
+        conn.commit()
+
+
+# Функция для удаления пользователя из черного списка
+async def unban_user(user_id):
+    # Проверяем, есть ли пользователь в базе данных
+    if await is_banned(user_id):
+        # Если есть, то обновляем значение столбца blacklist в False
+        sql = f"UPDATE users SET blacklist = False WHERE user_id = {user_id};"
+        cur.execute(sql)
+        conn.commit()
+
+
 bot = Bot(token=TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
@@ -33,7 +142,8 @@ async def set_default_commands(bot: Bot):
         BotCommand('get', 'Получение информации о себе из базы данных'),
         BotCommand('update', 'Обновление информации о себе в базе данных'),
         BotCommand('write', 'Записать информацию о себе в базу данных'),
-        BotCommand('delete', 'Удалить информацию о себе из базы данных')
+        BotCommand('delete', 'Удалить информацию о себе из базы данных'),
+        BotCommand('about', 'Информация о боте')
     ]
     return await bot.set_my_commands(commands=commands, scope=BotCommandScopeDefault())
                                      
@@ -45,11 +155,9 @@ class BlacklistMiddleware(BaseMiddleware):
         # Проверяем, находится ли пользователь в черном списке
         if await is_banned(message.from_user.id):
             # Если да, то прерываем цепочку обработки сообщения
-            try:
-                raise CancelHandler()
-            except Exception:
-                pass
+            raise CancelHandler()
         # Иначе продолжаем обработку сообщения как обычно
+
 
 # Регистрируем миддлварь в диспетчере
 dp.middleware.setup(BlacklistMiddleware())
@@ -60,8 +168,9 @@ async def is_admin(chat_id, user_id):
     member = await bot.get_chat_member(chat_id, user_id)
     return member.is_chat_admin()
 
+
 # Хэндлер для команды /ban
-@dp.message_handler(commands=["ban"])
+@dp.message_handler(commands=["ban"], chat_type=[ChatType.GROUP, ChatType.SUPERGROUP])
 async def ban_command(message: types.Message):
     # Проверяем, является ли отправитель команды администратором группы
     if await is_admin(message.chat.id, message.from_user.id):
@@ -133,6 +242,15 @@ async def checkban_command(message: types.Message):
         await message.reply("Вы не являетесь администратором группы.")
 
 
+@dp.message_handler(commands=['about'])
+async def send_about(message: types.Message):
+
+    text = (r'<b>Привет.</b> Я пет-проект являющийся ботом в телеграм реализованный <a href="https://t.me/legannyst/">Марком Клавишиным</a>' 
+        ' по курсу "<i>Код будущего</i>" от университета Иннополис.\nТы можешь получить информацию о командах введя <code>/help *команда*</code>.'
+        r' В принципе, ничего особенного - я просто могу записать твой user_id и nickname в базу'
+        ' и выслать их обратно тебе.\nНу и в случае чего - я имею функцию при которой админы группы заставят меня тебя игнорировать. Так что будь аккуратен.')
+    await message.answer(text)
+
 
 @dp.message_handler(commands=["start"])
 async def send_welcome(msg: types.Message):
@@ -193,7 +311,7 @@ async def get_user_data(message: types.Message):
 # Создаем фильтр с регулярным выражением для команды help
 help_filter = filters.RegexpCommandsFilter(regexp_commands=['help ( [a-z]*)'])
 
-# Фильтр для команды /start с использованием регулярного выражения
+# Фильтр для команды /start с использованием регулярного выражения  
 @dp.message_handler(regexp=r"^/help start$")
 async def start_command(message: types.Message):
     await message.answer('/start - поздороваться со мной и узнать, кто меня написал')
@@ -222,31 +340,33 @@ async def update_command(message: types.Message):
 async def get_command(message: types.Message):
     await message.answer('/get - получить информацию о себе из базы данных.')
 
-# инициализация обработчика для эхо сообщения
+# Создаем клавиатуру с двумя кнопками "ДА" и "НЕТ"
+keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+keyboard.add("ДА", "НЕТ")
+
+# Создаем клавиатуру для скрытия клавиатуры
+hide_keyboard = types.ReplyKeyboardRemove()
+
+# Регистрируем обработчик команды /keyboard
+@dp.message_handler(commands=["keyboard"])
+async def send_keyboard(message: types.Message):
+    # Отправляем сообщение с клавиатурой
+    await message.answer("Вы хотите продолжить?", reply_markup=keyboard)
+
+# Регистрируем обработчик нажатия кнопок
 @dp.message_handler()
-async def echo_message(message: types.Message):
-    # отправка эхо текста
-    await message.answer(message.text)
-
-
-@dp.message_handler(content_types=["animation", "photo", "video", "document", "sticker"])
-async def echo(message: types.Message):
-    # Получаем тип контента и файл ID
-    content_type = message.content_type
-    file_id = message[content_type].file_id
-    # Используем if content_type  для отправки обратно того же типа контента
-    if content_type == "animation":
-        await message.reply_animation(file_id)
-    elif content_type == "photo":
-        await message.reply_photo(file_id)
-    elif content_type ==  "video":
-        await message.reply_video(file_id)
-    elif content_type ==  "document":
-        await message.reply_document(file_id)
-    elif content_type ==  "sticker":
-        await message.reply_sticker(file_id)
+async def handle_buttons(message: types.Message):
+    # Проверяем текст сообщения
+    if message.text == "ДА":
+        # Отправляем сообщение "ДА" и скрываем клавиатуру
+        await message.reply("ДА", reply_markup=hide_keyboard)
+    elif message.text == "НЕТ":
+        # Отправляем сообщение "НЕТ" и скрываем клавиатуру
+        await message.reply("НЕТ", reply_markup=hide_keyboard)
     else:
-        await message.reply("Неизвестный тип контента")
+        # Отправляем сообщение "Не понимаю"
+        await message.reply("Не понимаю")
+
 
 
 if __name__ == "__main__":
